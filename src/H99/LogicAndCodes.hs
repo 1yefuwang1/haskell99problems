e0efb0977180d94895a6a699a93b69ef9bbc4940
1yefuwang1 <1yefuwang1@gmail.com>
2017-06-23 22:57:33 +0800

    Problem 48, 49

--- src/H99/LogicAndCodes.hs
+++ src/H99/LogicAndCodes.hs
@@ -1,5 +1,7 @@
 module H99.LogicAndCodes where
 
+import           Control.Monad (replicateM)
+
 {-
 Problem 46
 (**) Define predicates and/2, or/2, nand/2, nor/2, xor/2, impl/2 and equ/2 (for logical equivalence) which succeed or fail according to the result of their respective operations; e.g. and(A,B) will succeed, if and only if both A and B succeed.
@@ -23,6 +25,14 @@ True False True
 False True False
 False False False
 -}
+
+infixl 4 `or'`
+infixl 4 `nor'`
+infixl 5 `xor'`
+infixl 6 `and'`
+infixl 6 `nand'`
+infixl 3 `equ'`
+
 and' :: Bool -> Bool -> Bool
 and' True True = True
 and' _ _       = False
@@ -52,11 +62,11 @@ equ' _ _         = False
 
 table :: (Bool -> Bool -> Bool) -> [(Bool, Bool, Bool)]
 table f = do
-  a <- table'
-  b <- table'
+  a <- t
+  b <- t
   return (a, b, f a b)
   where
-    table' = [True, False]
+    t = [True, False]
 
 {-
 Problem 47
@@ -79,6 +89,84 @@ True False True
 False True False
 False False False
 -}
--- Haskell functions are automatically infix operators
+-- Haskell functions are infix operators, no work to do
 table' :: (Bool -> Bool -> Bool) -> [(Bool, Bool, Bool)]
 table' = table
+
+{-
+Problem 48
+(**) Truth tables for logical expressions (3).
+
+Generalize problem P47 in such a way that the logical expression may contain any number of logical variables. Define table/2 in a way that table(List,Expr) prints the truth table for the expression Expr, which contains the logical variables enumerated in List.
+
+Example:
+
+* (table (A,B,C) (A and (B or C) equ A and B or A and C))
+true true true true
+true true fail true
+true fail true true
+true fail fail true
+fail true true true
+fail true fail true
+fail fail true true
+fail fail fail true
+Example in Haskell:
+
+> tablen 3 (\[a,b,c] -> a `and'` (b `or'` c) `equ'` a `and'` b `or'` a `and'` c)
+-- infixl 3 `equ'`
+True  True  True  True
+True  True  False True
+True  False True  True
+True  False False True
+False True  True  True
+False True  False True
+False False True  True
+False False False True
+
+-- infixl 7 `equ'`
+True  True  True  True
+True  True  False True
+True  False True  True
+True  False False False
+False True  True  False
+False True  False False
+False False True  False
+False False False False
+-}
+tablen :: Int -> ([Bool] -> Bool) -> [[Bool]]
+tablen n f = do
+  c <- comb
+  return $ c ++ [f c]
+  where
+    comb = replicateM n [True, False]
+
+{-
+Problem 49
+(**) Gray codes.
+
+An n-bit Gray code is a sequence of n-bit strings constructed according to certain rules. For example,
+
+n = 1: C(1) = ['0','1'].
+n = 2: C(2) = ['00','01','11','10'].
+n = 3: C(3) = ['000','001','011','010',´110´,´111´,´101´,´100´].
+Find out the construction rules and write a predicate with the following specification:
+
+% gray(N,C) :- C is the N-bit Gray code
+Can you apply the method of "result caching" in order to make the predicate more efficient, when it is to be used repeatedly?
+
+Example in Haskell:
+
+P49> gray 3
+["000","001","011","010","110","111","101","100"]
+-}
+
+gray :: Int -> [[Char]]
+gray 1 = ["0", "1"]
+gray n =
+  if n <= 0
+    then error "n should be greater than 0"
+    else doGray
+  where
+    doGray = prepend' ('0') prev ++ prepend' ('1') (reverse prev)
+    prepend' x = map $ (x:)
+    prev = gray (n-1)
--- test/H99/LogicAndCodesSpec.hs
+++ test/H99/LogicAndCodesSpec.hs
@@ -3,6 +3,7 @@ module H99.LogicAndCodesSpec (spec) where
 import           H99.LogicAndCodes
 import           Test.Hspec
 
+
 spec :: Spec
 spec =
   describe "testing logic and codes problems" $ do
@@ -12,4 +13,9 @@ spec =
     it "Problem 47: print truth table continued" $ do
       table' (\a b -> (a `and'` (a `or'` b))) `shouldBe` [(True, True, True), (True, False, True), (False, True, False), (False, False, False)]
 
+    it "Problem 48: print truth table with multiple arguments" $ do
+      tablen 3 (\[a,b,c] -> a `and'` (b `or'` c) `equ'` a `and'` b `or'` a `and'` c) `shouldBe` [[True, True, True, True], [True, True, False, True], [True, False, True, True], [True, False, False, True], [False, True, True, True], [False, True, False, True], [False, False, True, True], [False, False, False, True]]
+
+    it "Problem 49: gray codes" $ do
+      gray 3 `shouldBe` ["000","001","011","010","110","111","101","100"]
 
