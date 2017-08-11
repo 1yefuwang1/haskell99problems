ae6c9acd890460a15fbd1f4abd5855aebbfecb9d
1yefuwang1 <1yefuwang1@gmail.com>
2017-08-12 00:52:57 +0800

    Improved comment

--- src/H99/Graphs.hs
+++ src/H99/Graphs.hs
@@ -256,10 +256,7 @@ prim g@(LabelledGraph ns es) = go [] ns [] g
       go (unvisited':visited) [n' | n' <- unvisited, unvisited' /= n'] (minAdjEdge:edges) g
 
 {-
-<<<<<<< HEAD
-=======
 Problem 85
->>>>>>> Added Problem 85: Graph isomorphism
 Graph isomorphism
 
 Two graphs G1(N1,E1) and G2(N2,E2) are isomorphic if there is a bijection f: N1 -> N2 such that for any nodes X,Y of N1, X and Y are adjacent if and only if f(X) and f(Y) are adjacent.
